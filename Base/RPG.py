e18709376012e4a377b6210a2752ef3884fea326
A-Jordan-95 <arj14b@my.fsu.edu>
2020-05-26 15:33:06 -0400

    update wallsoftreesandplayer

--- Base/RPG.py
+++ Base/RPG.py
@@ -9,24 +9,6 @@ SCREEN_HEIGHT = 600
 VIEWPORT_MARGIN = 40
 NUMBER_OF_TREES = 75
 
-"""class PlayerSprite(arcade.Sprite):
-
-    def update(self):
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-        if self.left < 0:
-            self.left = 0
-        elif self.right > SCREEN_WIDTH - 1:
-            self.right = SCREEN_WIDTH - 1
-
-        if self.bottom < 0:
-            self.bottom = 0
-        elif self.top > SCREEN_HEIGHT - 1:
-            self.top = SCREEN_HEIGHT - 1"""
-
-
-
 class RPG(arcade.Window):
 
     def __init__(self, width, height, title):
