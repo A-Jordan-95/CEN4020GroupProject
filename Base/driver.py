8508ef40681f5997c11b3a7c9738a9ec1cb99d36
Anthony Micciche <32377705+amicciche@users.noreply.github.com>
2020-07-07 15:24:50 -0400

    Moved the player to a "save"

--- Base/driver.py
+++ Base/driver.py
@@ -2,8 +2,14 @@
 
 import koronakombat as kk
 
-# Agent constructor is (hp, at, df, moveList, Name)
-hero = kk.Hero(10, 10, 8, 6, 5, 5, ['Attack', 'Smile'], 'Hero')
+f = open("playerData.txt", 'r')
+stats = f.readline().split()
+moveList = f.readline().split()
+name = f.readline().strip('\n')
+
+hero = kk.Hero(int(stats[0]),int(stats[1]),int(stats[2]),int(stats[3]),int(stats[4]),int(stats[5]), moveList, name)
+
+f.close()
 
 combat1 = kk.Kombat("Overworld", hero)
 
