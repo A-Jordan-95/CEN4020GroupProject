b1c7be818fa48ce059aeb41ea78860d6bab73592
FamousStephens <65720791+FamousStephens@users.noreply.github.com>
2020-07-30 21:45:09 -0400

    Add files via upload

--- Base/maps/Dialogue_Events.tsx
+++ Base/maps/Dialogue_Events.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.0" name="Dialogue_Events" tilewidth="128" tileheight="128" tilecount="10" columns="0">
+<tileset version="1.2" tiledversion="1.3.4" name="Dialogue_Events" tilewidth="128" tileheight="128" tilecount="21" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -40,22 +40,85 @@
   </properties>
   <image width="128" height="128" source="gun_tile.png"/>
  </tile>
+ <tile id="7">
+  <properties>
+   <property name="ID" type="int" value="100"/>
+  </properties>
+  <image width="128" height="128" source="tile3.png"/>
+ </tile>
  <tile id="8">
   <properties>
-   <property name="ID" type="int" value="6"/>
+   <property name="ID" type="int" value="200"/>
   </properties>
-  <image width="128" height="128" source="MantisEvent.png"/>
+  <image width="128" height="128" source="db_tile.PNG"/>
  </tile>
  <tile id="9">
-  <properties>
-   <property name="ID" type="int" value="51"/>
-  </properties>
-  <image width="128" height="128" source="ToiletPaper_Tile.png"/>
+  <image width="128" height="128" source="grass.png"/>
  </tile>
  <tile id="10">
   <properties>
-   <property name="ID" type="int" value="52"/>
+   <property name="ID" type="int" value="200"/>
   </properties>
-  <image width="128" height="128" source="HydroC_Tile.png"/>
+  <image width="128" height="128" source="../Inventory/shotgun.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="ID" type="int" value="420"/>
+  </properties>
+  <image width="128" height="128" source="chad.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="ID" type="int" value="421"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/face_mask.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="ID" type="int" value="422"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/Noodle_hat.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="ID" type="int" value="424"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/energy_drink.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="ID" type="int" value="425"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/ubereats.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="ID" type="int" value="426"/>
+  </properties>
+  <image width="96" height="96" source="../Inventory/hydroxychloriquine.png"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="ID" type="int" value="102"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/Mp40.png"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="ID" type="int" value="500"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/helmet.png"/>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="ID" type="int" value="506"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/jeans.png"/>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="ID" type="int" value="505"/>
+  </properties>
+  <image width="128" height="128" source="../Inventory/glove.png"/>
  </tile>
 </tileset>
